52334060c3048cfeadebc1bc663ae3d71fc23027
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2020-12-19 07:57:40 +0900

    DataBinding 未完状態

--- app/src/main/java/com/android/databindingcheckbox/MainActivity.kt
+++ app/src/main/java/com/android/databindingcheckbox/MainActivity.kt
@@ -2,10 +2,39 @@ package com.android.databindingcheckbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.Bindable
 
 class MainActivity : AppCompatActivity() {
+    private var text: String = "チェックなし"
+    private var isChecked: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onChangeTextClick(isChecked: Boolean) {
+
+        text = if (isChecked) {
+            "チェックあり"
+        } else {
+            "チェックなし"
+        }
+    }
+
+    fun setChecked(isChecked: Boolean) {
+
+    }
+
+    fun isChecked(): Boolean {
+        return isChecked
+    }
+
+    fun setText(text: String) {
+        this.text = text
+    }
+
+    fun getText(): String {
+        return text
     }
 }
